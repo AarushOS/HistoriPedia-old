3d21722e88fdd2f102d7f078aa00523870df47f0
Titan <80444511+Titan-Codes@users.noreply.github.com>
2022-04-21 20:17:23 +0530

    made a few changes.

--- back-end/app.js
+++ back-end/app.js
@@ -6,3 +6,12 @@ const ejs = require("ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("style"));
+
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
+
+app.listen(3000, function(){
+  console.log("Server running on port 3000");
+});
