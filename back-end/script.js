96413fde8e0aef82aa0f9384894c32d6a4206594
AarushOS <79082249+AarushOS@users.noreply.github.com>
2022-04-30 10:33:53 +0530

    Update script.js

--- back-end/script.js
+++ back-end/script.js
@@ -6,35 +6,51 @@ const suggBox = document.getElementById("autocom-box");
 inputBox.onkeyup = (e) => {
     let userData = e.target.value;
     let emptyArray = [];
-    if(userData){
+    if (userData) {
         emptyArray = suggestions.filter((data) => {
             return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+
         });
+        console.log(emptyArray);
         emptyArray = emptyArray.map((data) => {
             return data = '<li>' + data + '</li>';
         });
-    }
-    else{
+        searchWrapper.classList.add("active");
+        showSuggestions(emptyArray);
+        createDropDown(emptyArray);
 
+    } else {
+        searchWrapper.classList.remove("active");
     }
-    showSuggestions(emptyArray);
-    searchWrapper.classList.add("active");
+
 }
 
-function check(list){
-    if (list == "<li>undefined</li>"){
+function createDropDown(list) {
+    const suggBox2 = document.getElementById("autocom-box");
+    if (list.length > 8) {
+        console.log("Do this... ");
+        suggBox2.style.transform = "translate(-1.3%, -1%)";
+
+    } else {
+        console.log("Stop operation....");
+        suggBox2.style.transform = "translate(-1.35% , -4% )";
+    }
+}
+
+function check(list) {
+    if (list == "<li>undefined</li>") {
         suggBox.innerHTML = "Search for any article";
     }
 }
 
-function showSuggestions(list){
+function showSuggestions(list) {
     let listData;
-    if (!list.length){
-        //userValue = inputBox.value;
-        //listData = '<li>' + userValue + '</li>';
-    }
-    else{
+    if (!list.length) {
+        userValue = inputBox.value;
+        listData = '<li>' + userValue + '</li>';
+    } else {
         listData = list.join('');
+
     }
     suggBox.innerHTML = listData;
 }
