ee917a001f476438e640eb25f866d3424a936c4a
AarushOS <79082249+AarushOS@users.noreply.github.com>
2022-05-12 18:57:02 +0530

    Update Gsignin.js

--- back-end/Gsignin.js
+++ back-end/Gsignin.js
@@ -6,3 +6,9 @@ function onSignIn(googleUser) {
     $(".g-signin2").css("display", "none");
         
 }
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
