6de22e12a843f74676911cd45df3a1cf531a5bdb
AarushOS <79082249+AarushOS@users.noreply.github.com>
2022-05-12 19:22:12 +0530

    Update Gsignin.js

--- back-end/Gsignin.js
+++ back-end/Gsignin.js
@@ -1,11 +1,10 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    $("#name").text(profile.getName());
-    $("#image").attr('src', profile.getImageUrl());
+    var name = $("#name").text(profile.getName());
+    var img = $("#image").attr('src', profile.getImageUrl());
     $(".data").css("display", "block");
     $(".g-signin2").css("display", "none");
-        
-}
+} 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
