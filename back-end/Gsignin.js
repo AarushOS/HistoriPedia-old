fe4a73deb0a5175c0eb7eb1803a254e642875dd2
AarushOS <79082249+AarushOS@users.noreply.github.com>
2022-05-12 18:48:54 +0530

    Add files via upload

---
+++ back-end/Gsignin.js
@@ -0,0 +1,8 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    $("#name").text(profile.getName());
+    $("#image").attr('src', profile.getImageUrl());
+    $(".data").css("display", "block");
+    $("g-signin2").css("display", "none");
+        
+}
